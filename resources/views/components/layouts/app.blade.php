068ccece3a5fcf572b52cdfd62ba184cb4f9fdca
Luis Parrado <luisprmat@gmail.com>
2023-11-19 17:51:29 -0500

    Install And Configure TailwindCSS

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Build filament components</title>
+
+    @filamentStyles
+    @vite('resources/css/app.css')
 </head>
 <body>
     {{ $slot }}
+
+    @filamentScripts
+    @vite('resources/js/app.js')
+    @livewire('notifications')
 </body>
 </html>
