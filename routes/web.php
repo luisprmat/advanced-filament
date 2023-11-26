549611d953dcc9e1e7ae0d5526860d918878da9e
Luis Parrado <luisprmat@gmail.com>
2023-11-26 00:23:30 -0500

    Add Livewire component for table

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Livewire\DemoForm;
 use App\Livewire\DemoInfolist;
+use App\Livewire\DemoTable;
 use App\Livewire\TestForm;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/demo', TestForm::class);
 Route::get('/form', DemoForm::class);
 Route::get('/infolist', DemoInfolist::class);
+Route::get('/table', DemoTable::class);
