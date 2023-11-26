28a527266534136437edfa5e08738e587b9aaf1c
Luis Parrado <luisprmat@gmail.com>
2023-11-26 12:17:40 -0500

    Setting up a Plugin

---
+++ packages/filament-toolkit/src/FilamentToolkitServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Luisprmat\FilamentToolkit;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentToolkitServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('filament-toolkit')
+            ->hasViews();
+    }
+}
