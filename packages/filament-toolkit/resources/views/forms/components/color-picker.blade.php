a23c90ed27eca539783bc5fbddd108a814e2dbf2
Luis Parrado <luisprmat@gmail.com>
2023-11-26 19:25:25 -0500

    Using Async Alpine Components

--- packages/filament-toolkit/resources/views/forms/components/color-picker.blade.php
+++ packages/filament-toolkit/resources/views/forms/components/color-picker.blade.php
@@ -1,3 +1,5 @@
+@php use Filament\Support\Facades\FilamentAsset; @endphp
+
 <x-dynamic-component
     :component="$getFieldWrapperView()"
     :field="$field"
@@ -5,20 +7,12 @@
     @php
         $width = $getWidth();
     @endphp
-    <div
-        x-data="{ state: $wire.$entangle('{{ $getStatePath() }}') }"
-        x-init="
-            const colorPicker = new iro.ColorPicker($refs.picker, {
-                @if ($width)
-                    width: @js($width),
-                @endif
-                color: state
-            })
 
-            colorPicker.on('color:change', (color) => {
-                state = color.hexString
-            })
-        "
+    <div
+        ax-load
+	    ax-load-src="{{ FilamentAsset::getAlpineComponentSrc('color-picker', 'luisprmat/filament-toolkit') }}"
+        x-ignore
+        x-data="colorPicker({ state: $wire.entangle('{{ $getStatePath() }}'), width: @js($width) })"
     >
         <div
             wire:ignore
